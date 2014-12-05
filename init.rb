2735096d7ff87619f184a6d0ae64d323e6c80705
Tobias Fischer <tobias.carl.fischer@gmail.com>
2014-12-05 11:00:02 +0100

    version 0.0.4

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_lightbox2 do
   name 'Redmine Lightbox 2'
   author 'G.K. & Tobias Fischer'
   description 'This plugin lets you preview image, pdf and swf attachments in a lightbox.'
-  version '0.0.3'
+  version '0.0.4'
   url 'https://github.com/paginagmbh/redmine_lightbox2'
   requires_redmine :version_or_higher => '2.6.0'
 end
