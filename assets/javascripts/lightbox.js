a104bd4572c49e5c20da3f3f34295e74ee6f5717
Tobias Fischer <tobias.carl.fischer@gmail.com>
2015-01-18 23:43:34 +0100

    corrected code indentation

--- assets/javascripts/lightbox.js
+++ assets/javascripts/lightbox.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-    $("div.attachments a.lightbox, div.attachments a.swf").fancybox({
+	$("div.attachments a.lightbox, div.attachments a.swf").fancybox({
 			prevEffect		: 'none',
 			nextEffect		: 'none',
-    		openSpeed		:	400, 
-    		closeSpeed		:	200
-  		});
+			openSpeed		: 400, 
+			closeSpeed		: 200
+		});
 
     $("div.attachments a.pdf").fancybox({
 			prevEffect		: 'none',
 			nextEffect		: 'none',
-    		openSpeed		:	400, 
-    		closeSpeed		:	200,
-			width			: '100%',
-			height			: '100%',
+			openSpeed		: 400, 
+			closeSpeed		: 200,
+			width			: '90%',
+			height			: '90%',
 			autoSize		: true,
 			iframe : {
 				preload: false
