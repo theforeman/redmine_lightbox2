01009e61c7edd559ffc2e7b96b29fa9a9728c79e
Tobias Fischer <tobias.carl.fischer@gmail.com>
2015-01-18 19:07:38 +0100

    bugfix for pdf display inline // fixes #4

--- lib/hooks/view_layouts_base_html_head_hook.rb
+++ lib/hooks/view_layouts_base_html_head_hook.rb
@@ -1,4 +1,4 @@
-module RedmineLightBox
+module RedmineLightbox2
   module Hooks
     class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
