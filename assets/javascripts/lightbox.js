453f170fd420644fe6d9855dd32b988586c6a78f
Tobias Fischer <tobias.carl.fischer@gmail.com>
2015-11-21 20:43:09 +0100

    add lightbox to attachment links (via icon), image attachment links in journal details and to thumbnails // refs #16

--- assets/javascripts/lightbox.js
+++ assets/javascripts/lightbox.js
@@ -1,15 +1,33 @@
 $(document).ready(function() {
-	$("div.attachments a.lightbox, div.attachments a.swf").fancybox({
+    
+    // modify thumbnail links and add filename to url to support fancybox preview
+    $("div.journal a.thumbnail").attr('href', function(i, v){
+      return v + '/' + $(this).attr('title');
+    });
+    
+	$("div.attachments a.lightbox," +
+      "div.attachments a.lightbox_preview," +
+	  "div.journal ul.details a[href$='.png']," +
+	  "div.journal ul.details a[href$='.PNG']," +
+	  "div.journal ul.details a[href$='.jpg']," +
+	  "div.journal ul.details a[href$='.JPG']," +
+	  "div.journal ul.details a[href$='.jpeg']," +
+	  "div.journal ul.details a[href$='.JPEG']," +
+	  "div.journal ul.details a[href$='.gif']," +
+	  "div.journal ul.details a[href$='.GIF']," +
+	  "div.journal a.thumbnail," +
+	  "div.attachments a.swf").fancybox({
 			prevEffect		: 'none',
 			nextEffect		: 'none',
-			openSpeed		: 400, 
+			openSpeed		: 400,
 			closeSpeed		: 200
+			}
 		});
 
     $("div.attachments a.pdf").fancybox({
 			prevEffect		: 'none',
 			nextEffect		: 'none',
-			openSpeed		: 400, 
+			openSpeed		: 400,
 			closeSpeed		: 200,
 			width			: '90%',
 			height			: '90%',
