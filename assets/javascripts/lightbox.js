8ef1d979efa619450a2db5b83e0b59a6b139fa5b
Tobias Fischer <tobias.carl.fischer@gmail.com>
2019-01-04 16:13:58 +0100

    Refactoring & BMP filetype support // fixes #63

--- assets/javascripts/lightbox.js
+++ assets/javascripts/lightbox.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
   // the file extension regex matching on supported image and pdf types
-  var extensionRegex = /\.(png|jpe?g|gif|pdf)$/i;
+  var extensionRegexImage = /\.(png|jpe?g|gif|bmp)$/i;
+  var extensionRegexAll = /\.(png|jpe?g|gif|bmp|pdf)$/i;
 
   // modify thumbnail links in wiki content -> add filename from ./img/@alt to url to support fancybox preview
   $("div.wiki a.thumbnail").attr('href', function(i, v){
-    return v.replace(/\/attachments\/(\d+)/g,'/attachments/download/$1') + '/' + $(this).children('img').attr('alt').replace(/(.*\.(png|jpe?g|gif))(\s\(.*\))?/gi,'$1');
+    return v.replace(/\/attachments\/(\d+)/g,'/attachments/download/$1') + '/' + $(this).children('img').attr('alt').replace(/(.*\.(png|jpe?g|gif|bmp))(\s\(.*\))?/gi,'$1');
   });
 
   // modify thumbnails and magnifier links in journal details -> add filename to url to support fancybox preview
   $("div.journal div.thumbnails a, div.journal ul.details li a:not([title])").attr('href', function(i, v){
-    if($(this).attr('href').match(extensionRegex)) {
+    if($(this).attr('href').match(extensionRegexAll)) {
       return v.replace(/\/attachments\/(\d+)/g,'/attachments/download/$1');
     } else {
       return v;
@@ -18,7 +19,7 @@ $(document).ready(function() {
 
   // add a magnifier icon before download icon for images and pdf
   $("div.journal ul.details li a.icon-download").each(function(i, obj) {
-    if($(this).attr('href').match(extensionRegex)) {
+    if($(this).attr('href').match(extensionRegexAll)) {
       var icon = $(this).clone().attr('class', function(i, v){
         return v.replace(/-download/g,'-magnifier');
       });
@@ -37,7 +38,7 @@ $(document).ready(function() {
     // Also support PDF preview in lightbox
     var isPdf = filename.match(/\.pdf$/i);
     // only apply thumbnail class to image and pdf links
-    if(filename.match(extensionRegex)) {
+    if(filename.match(extensionRegexAll)) {
       $(this)
         .attr('class', 'thumbnail')
         .attr('data-fancybox-type', isPdf ? 'iframe' : 'image')
@@ -64,7 +65,7 @@ $(document).ready(function() {
     // Also support PDF preview in lightbox
     var isPdf = filename.match(/\.pdf$/i);
     // only apply thumbnail class to image and pdf links
-    if(filename.match(extensionRegex)) {
+    if(filename.match(extensionRegexAll)) {
       $(this)
         .addClass('lightbox')
         .attr('data-fancybox-type', isPdf ? 'iframe' : 'image')
@@ -75,42 +76,34 @@ $(document).ready(function() {
   });
 
   // Add Fancybox to image links
-  $("div.attachments a.lightbox," +
-    "div.attachments a.lightbox_preview," +
-    "div.journal ul.details a[href$='.png']:not(.icon-download)," +
-    "div.journal ul.details a[href$='.PNG']:not(.icon-download)," +
-    "div.journal ul.details a[href$='.jpg']:not(.icon-download)," +
-    "div.journal ul.details a[href$='.JPG']:not(.icon-download)," +
-    "div.journal ul.details a[href$='.jpeg']:not(.icon-download)," +
-    "div.journal ul.details a[href$='.JPEG']:not(.icon-download)," +
-    "div.journal ul.details a[href$='.gif']:not(.icon-download)," +
-    "div.journal ul.details a[href$='.GIF']:not(.icon-download)," +
-    "div.journal div.thumbnails a," +
-    "div.wiki a.thumbnail," +
-    "div.attachments a.swf," +
-    ".avatar a").fancybox({
-      prevEffect    : 'none',
-      nextEffect    : 'none',
-      openSpeed     : 300,
-      closeSpeed    : 150
+  $("div.attachments a.lightbox")
+  .add("div.attachments a.lightbox_preview")
+  .add("div.journal ul.details a:not(.icon-download)").filter((index,elem) => $(elem).attr('href').match(extensionRegexImage))
+  .add("div.journal div.thumbnails a")
+  .add("div.wiki a.thumbnail")
+  .add("avatar a")
+  .fancybox({
+    prevEffect    : 'none',
+    nextEffect    : 'none',
+    openSpeed     : 300,
+    closeSpeed    : 150
   });
 
-    // Add Fancybox to PDF links
-  $("div.attachments a.pdf," +
-    "div.journal ul.details a[href$='.pdf']:not(.icon-download)," +
-    "div.journal ul.details a[href$='.PDF']:not(.icon-download)," +
-    "div.journal div.thumbnails a[href$='.pdf']," +
-    "div.journal div.thumbnails a[href$='.PDF']").fancybox({
-      type          : 'iframe',
-      prevEffect    : 'none',
-      nextEffect    : 'none',
-      openSpeed     : 300,
-      closeSpeed    : 150,
-      width         : '90%',
-      height        : '90%',
-      autoSize      : true,
-      iframe : {
-        preload: false
-      }
+  // Add Fancybox to PDF links
+  $("div.attachments a.pdf")
+  .add("div.journal ul.details a:not(.icon-download)").filter((index,elem) => $(elem).attr('href').match(/\.pdf$/i))
+  .add("div.journal div.thumbnails a").filter((index,elem) => $(elem).attr('href').match(/\.pdf$/i))
+  .fancybox({
+    type          : 'iframe',
+    prevEffect    : 'none',
+    nextEffect    : 'none',
+    openSpeed     : 300,
+    closeSpeed    : 150,
+    width         : '90%',
+    height        : '90%',
+    autoSize      : true,
+    iframe : {
+      preload: false
+    }
   });
 });
